a6f11631e6451dfbd4f18c7141ce5750ef8cbcc9
Majid Garmaroudi <majid.garmaroudi@klarna.com>
2014-12-17 23:41:14 +0100

    Some gulp functionalities

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,25 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    exec = require('child_process').exec;
 
-gulp.task('default', function() {
+gulp.task('install', function(cb) {
+    console.log('Install npm modules');
+    exec('npm install', function (err) {
+        if (err) return cb(err);
 
+        console.log('Install bower modules');
+        exec('bower install', function (err) {
+            if (err) return cb(err);
+            cb();
+        });
+    });
 });
+
+gulp.task('run', ['install'], function(cb) {
+    console.log('Running server');
+    exec('node main.js', function (err) {
+        if (err) return cb(err);
+            cb();
+    });
+});
+
+gulp.task('default', ['install', 'run']);
